de6131c5329961fb0f6e6c816d49a157f58bc1aa
Bylvin <56466617+Bylvin@users.noreply.github.com>
2022-10-27 13:10:12 +0700

    Add feature card list

--- wallet-app/src/app/home/home.page.ts
+++ wallet-app/src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+// import error masalah IDE
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  // features
+  features = [
+    {id: 1, name: 'Top Up', icon: 'assets/icons/topup.png', page: ''},
+    {id: 2, name: 'Tarik Saldo', icon: 'assets/icons/withdrawal.png', page: ''},
+    {id: 3, name: 'Kirim', icon: 'assets/icons/send.png', page: ''},
+    {id: 4, name: 'Bayar', icon: 'assets/icons/debit-card.png', page: ''}
+  ];
 
   constructor() {}
 
